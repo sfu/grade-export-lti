e6b57bd436a4749beaa7dc434fc4e847f93b0686
Reza <rshariat@sfu.ca>
2017-09-29 15:54:57 -0700

    Added GradingScheme form partial

--- app/controllers/grading_standards_controller.rb
+++ app/controllers/grading_standards_controller.rb
@@ -1,6 +1,6 @@
 class GradingStandardsController < ApplicationController
   def show
-    @grading_standard = current_user.grading_standards.first
+    @grading_standards = current_user.grading_standards
   end
 
   def new
@@ -8,5 +8,18 @@ class GradingStandardsController < ApplicationController
   end
 
   def create
+    @grading_standard = current_user.grading_standards.new(grading_standard_params)
+    if @grading_standard.save
+      flash[:success] = "New Grading Standard has been added"
+      redirect_to @grading_standard
+    else
+      render 'new'
+    end
+  end
+
+  private
+  private
+  def grading_standard_params
+    params.required(:grading_standard).permit(:title, :grading_scheme)
   end
 end
