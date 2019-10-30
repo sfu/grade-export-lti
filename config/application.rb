9d94045207af0a70b17b5556212a72015790f34c
Graham Ballantyne <grahamb@sfu.ca>
2019-10-30 13:26:52 -0700

    Make the service domain configurable

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module GradeExportApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.redis = config_for(:redis)
+    config.domain = config_for(:domain)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,4 @@
 BASE_URL           = 'web.canvaslms.docker'
-GET_TOKEN_ENDPOINT = 'http://0.0.0.0:3001/get_token'
 
 module CourseDetail
   TERM    = 0
