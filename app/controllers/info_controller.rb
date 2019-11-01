682da64d88da8af5014011d5f72b4c47916e2382
Graham Ballantyne <grahamb@sfu.ca>
2019-11-01 10:28:25 -0700

    Add release version info to health_check.json

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -4,8 +4,19 @@ class InfoController < ApplicationController
     respond_to do |format|
       format.html { render plain: 'ok'}
       format.json { render json: {
-        status: 'ok'
+        status: 'ok',
+        release: release
       }}
+    end
+  end
+
+  private
+  def release
+    return @release if defined?(@release)
+    @revision = if File.file?(Rails.root+"RELEASE")
+      File.readlines(Rails.root+"RELEASE").first.try(:strip)
+    else
+      nil
     end
   end
 
